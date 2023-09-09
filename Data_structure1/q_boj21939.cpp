9c686bcd1a41fdda4580e5f0b29dea5c3b863afe
goodbyebada <goodbyebada@naver.com>
2023-09-10 01:54:12 +0900

    rebase

--- Data_structure1/q_boj21939.cpp
+++ Data_structure1/q_boj21939.cpp
@@ -1,104 +1,80 @@
-<<<<<<< HEAD
-#include <bits/stdc++.h>
-using namespace std;
- 
-set<pair<int, int>> db;
-//set{} => binary search tree¸¦ °¡Áö°í ÀÖ´Ù
- 
-map<int, int> dict;
-//key, value  
-
-//´ä °Ë»öÇÔ 
-// SetÀº ¼ø¼­°¡ º¸ÀåµÇÁö ¾Ê°í Áßº¹À» Çã¿ëÇÏÁö ¾Ê´Â´Ù
-// MapÀº ¼ø¼­°¡ º¸ÀåµÇÁö ¾Ê°í, Key Áßº¹Àº Çã¿ëÇÏÁö ¾ÊÁö¸¸ ValueÀÇ Áßº¹Àº Çã¿ëµÈ´Ù.
- 
- 
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
- 
-    int n, m; 
-	cin >> n;
-	
-    for(int i = 0; i < n; i++){
-        int p, l; 
-        // p = ¹®Á¦  ¹øÈ£
-		// l = ¹®Á¦ ³­ÀÌµµ  
-		cin >> p >> l;
-        db.insert({l, p});
-        dict[p] = l;
-        // dict[¹®Á¦¹øÈ£] = ¹®Á¦ ³­ÀÌµµ ==> ¹®Á¦¹øÈ£·Î ¹®Á¦ ³­ÀÌµµ¸¦ Ã£´Â´Ù O(logN)  
-    }
-    
-    cin >> m;
-    
-    for(int i = 0; i < m; i++){
-        string op; 
-		cin >> op;
- 
-        if(op == "add"){
-            int p, l; 
-			cin >> p >> l;
-            db.insert({l, p});            
-            dict[p] = l;
-            // dict[¹®Á¦¹øÈ£] = ¹®Á¦ ³­ÀÌµµ 
-        }else if(op == "recommend"){
-            int x; 
-			cin >> x;
-            if(x == -1){    // °¡Àå ½¬¿î ¹®Á¦ÀÇ ¹øÈ£¸¦ Ãâ·Â
-                cout << (*db.begin()).second << "\n";
-                // db.begin() => ÁÖ¼Ò°ª 
-				//  *db.begin() => °ª! 
-            }else{  // x == 1, °¡Àå ¾î·Á¿î ¹®Á¦ÀÇ ¹øÈ£¸¦ Ãâ·Â
-                cout << (*prev(db.end())).second << "\n";
-                
-			//prev() : ±âÁØ iterator¿¡¼­ ¿øÇÏ´Â °Å¸®(-nÂ÷) °Å¸®ÀÇ iterator¸¦ ¹İÈ¯ÇÏ´Â ÇÔ¼ö                
-            }
-        }else{  
-		// op == "solved"
-            int p; 
-			cin >> p;
-            db.erase({dict[p], p});
-            //  ´Ù setÀ¸·Î ÇÏ¸é ¾ÈµÊ? solved ¹®Á¦ ÀÖ´ÂÁö Å½»öÇÒ¶§ {l,p} ·Î Ã£¾Æ¾ßÇÏ´Âµ¥
-			// lÀ» ¸ğ¸£´Ï±î ¸ø Ã£À½ => mapÀº m[key]  = value; ²ÃÀÌ´Ï±î p·Î l¸¦ ¾Ë ¼ö ÀÖ´Ù 
-			//±×·¸±â ‹š¹®¿¡ mapÀ» ¾´´Ù³×  
-            dict.erase(p);
-            //solved ÇßÀ¸´Ï dict¿¡¼­µµ »èÁ¦  
-        }
-    }
-}
-=======
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
-#include <stack>
 #include <set>
-
+#include <map>
 using namespace std;
 
-/*
-ë‘˜ ë‹¤ ë ˆë“œë¸”ë™íŠ¸ë¦¬ ì‚½ì…/ì‚­ì œ/íƒìƒ‰ ì‹œê°„ ë³µì¡ë„ log(n)
-set ì‚¬ìš© -> ì¤‘ë³µëœ ê°’ì„ ê°€ì§€ì§€ ì•ŠëŠ”ë‹¤
+set<pair<int, int>> db;
+// set{} => binary search treeï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ö´ï¿½
 
-1. add p l
-2. solved p  -> ë¬¸ì œ ì‚­ì œ
-3. recommand x -> ë¬¸ì œ ì¶œë ¥
+map<int, int> dict;
+// key, value
 
-*/
-
-set<int> list;
-
-int boj()
-{
-}
+// ï¿½ï¿½ ï¿½Ë»ï¿½ï¿½ï¿½
+//  Setï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ê°ï¿½ ï¿½ßºï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ê´Â´ï¿½
+//  Mapï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ê°ï¿½, Key ï¿½ßºï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ Valueï¿½ï¿½ ï¿½ßºï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½È´ï¿½.
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    boj();
-    return 0;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int p, l;
+        // p = ï¿½ï¿½ï¿½ï¿½  ï¿½ï¿½È£
+        // l = ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½Ìµï¿½
+        cin >> p >> l;
+        db.insert({l, p});
+        dict[p] = l;
+        // dict[ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½È£] = ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½Ìµï¿½ ==> ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½È£ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½Ìµï¿½ï¿½ï¿½ Ã£ï¿½Â´ï¿½ O(logN)
+    }
+
+    cin >> m;
+
+    for (int i = 0; i < m; i++)
+    {
+        string op;
+        cin >> op;
+
+        if (op == "add")
+        {
+            int p, l;
+            cin >> p >> l;
+            db.insert({l, p});
+            dict[p] = l;
+            // dict[ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½È£] = ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½Ìµï¿½
+        }
+        else if (op == "recommend")
+        {
+            int x;
+            cin >> x;
+            if (x == -1)
+            { // ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½È£ï¿½ï¿½ ï¿½ï¿½ï¿½
+                cout << (*db.begin()).second << "\n";
+                // db.begin() => ï¿½Ö¼Ò°ï¿½
+                //  *db.begin() => ï¿½ï¿½!
+            }
+            else
+            { // x == 1, ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½È£ï¿½ï¿½ ï¿½ï¿½ï¿½
+                cout << (*prev(db.end())).second << "\n";
+
+                // prev() : ï¿½ï¿½ï¿½ï¿½ iteratorï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½Ï´ï¿½ ï¿½Å¸ï¿½(-nï¿½ï¿½) ï¿½Å¸ï¿½ï¿½ï¿½ iteratorï¿½ï¿½ ï¿½ï¿½È¯ï¿½Ï´ï¿½ ï¿½Ô¼ï¿½
+            }
+        }
+        else
+        {
+            // op == "solved"
+            int p;
+            cin >> p;
+            db.erase({dict[p], p});
+            //  ï¿½ï¿½ setï¿½ï¿½ï¿½ï¿½ ï¿½Ï¸ï¿½ ï¿½Èµï¿½? solved ï¿½ï¿½ï¿½ï¿½ ï¿½Ö´ï¿½ï¿½ï¿½ Å½ï¿½ï¿½ï¿½Ò¶ï¿½ {l,p} ï¿½ï¿½ Ã£ï¿½Æ¾ï¿½ï¿½Ï´Âµï¿½
+            // lï¿½ï¿½ ï¿½ğ¸£´Ï±ï¿½ ï¿½ï¿½ Ã£ï¿½ï¿½ => mapï¿½ï¿½ m[key]  = value; ï¿½ï¿½ï¿½Ì´Ï±ï¿½ pï¿½ï¿½ lï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½ ï¿½Ö´ï¿½
+            // ï¿½×·ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ mapï¿½ï¿½ ï¿½ï¿½ï¿½Ù³ï¿½
+            dict.erase(p);
+            // solved ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ dictï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
+        }
+    }
 }
->>>>>>> cc0a11e (ì•Œê³ ë¦¬ì¦˜)
