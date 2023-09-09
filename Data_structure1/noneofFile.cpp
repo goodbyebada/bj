3846171c50e5e097a8e81ade23297d9bcc9cfdc1
goodbyebada <goodbyebada@naver.com>
2023-09-10 01:57:52 +0900

    Add

---
+++ Data_structure1/noneofFile.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+using namespace std;
+
+/*
+
+*/
+
+int boj()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    boj();
+    return 0;
+}
