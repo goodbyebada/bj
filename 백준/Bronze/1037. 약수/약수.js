c248ce5311b94e0e0f246d0fa9a9fe2ca6684f61
goodbyebada <90432681+goodbyebada@users.noreply.github.com>
2024-12-09 22:29:14 +0900

    [Bronze I] Title: 약수, Time: 108 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.js
@@ -0,0 +1,58 @@
+/**
+ * 약수
+ * 브론즈 1
+ */
+
+function GCD(a, b) {
+  let gcdA;
+  let gcdB;
+
+  if (a > b) {
+    gcdA = a;
+    gcdB = b;
+  } else {
+    gcdA = b;
+    gcdB = a;
+  }
+
+  while (1) {
+    let result = gcdA % gcdB;
+
+    if (result == 0) {
+      return gcdB;
+    }
+
+    gcdA = gcdB;
+    gcdB = result;
+  }
+}
+
+function LCM(b, a) {
+  const gcd = GCD(a, b);
+  return (a * b) / gcd;
+}
+
+function main() {
+  const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./text.txt")
+    .toString()
+    .split("\n");
+
+  const numbers = input[1]
+    .split(" ")
+    .map(Number)
+    .sort(function (a, b) {
+      return a - b;
+    });
+
+  console.log(numbers[0] * numbers[numbers.length - 1]);
+
+  //   let result = numbers.reduce((acc, curr) => LCM(acc, curr));
+
+  //   if (result == numbers[numbers.length - 1]) {
+  //     result *= numbers[0];
+  //   }
+  //   console.log(result);
+}
+
+main();
