ae9a3aef21684dd4a4a8d73545f5655872a36108
goodbyebada <90432681+goodbyebada@users.noreply.github.com>
2024-12-17 23:10:36 +0900

    [Bronze I] Title: 최소공배수, Time: 192 ms, Memory: 12020 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.js
@@ -0,0 +1,26 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((elem) => elem.split(" "));
+
+const n = input.shift();
+
+function GCD(a, b) {
+  while (b !== 0) {
+    const tmp = b;
+    b = a % b;
+    a = tmp;
+  }
+
+  return a;
+}
+
+for (let i = 0; i < +n; i++) {
+  let a = Number(input[i][0]);
+  let b = Number(input[i][1]);
+
+  let gcd = GCD(a, b);
+  console.log((a * b) / gcd);
+}
