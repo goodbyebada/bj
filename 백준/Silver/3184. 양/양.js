5d8aedadc6142826d795d7360a1337e2cca54653
goodbyebada <90432681+goodbyebada@users.noreply.github.com>
2025-01-03 21:34:05 +0900

    [Silver I] Title: 양, Time: 208 ms, Memory: 20268 KB -BaekjoonHub

---
+++ 백준/Silver/3184. 양/양.js
@@ -0,0 +1,68 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [r, c] = input[0].split(" ").map(Number);
+const board = input.splice(1).map((elem) => elem.split(""));
+const visited = Array.from(Array(r), () => Array(c).fill(false));
+
+const dx = [-1, 0, 1, 0];
+const dy = [0, 1, 0, -1];
+let oCnt = 0;
+let vCnt = 0;
+
+for (let row = 0; row < r; row++) {
+  for (let col = 0; col < c; col++) {
+    if (visited[row][col]) continue;
+
+    const [o, v] = DFS(row, col);
+
+    oCnt += o;
+    vCnt += v;
+  }
+}
+
+console.log(oCnt, vCnt);
+
+function DFS(initX, initY) {
+  const stack = [];
+  let oCnt = 0;
+  let vCnt = 0;
+
+  stack.push([initX, initY]);
+
+  while (stack.length !== 0) {
+    const [currX, currY] = stack.pop();
+    if (visited[currX][currY]) continue;
+    if (board[currX][currY] === "o") {
+      oCnt++;
+    }
+    if (board[currX][currY] === "v") vCnt++;
+
+    visited[currX][currY] = true;
+
+    for (let dir = 0; dir < 4; dir++) {
+      let mx = currX + dx[dir];
+      let my = currY + dy[dir];
+
+      if (mx < 0 || my < 0 || mx >= r || my >= c) continue;
+      if (visited[mx][my]) {
+        continue;
+      }
+
+      if (board[mx][my] === "#") continue;
+
+      stack.push([mx, my]);
+    }
+  }
+
+  if (oCnt > vCnt) {
+    vCnt = 0;
+  } else {
+    oCnt = 0;
+  }
+
+  return [oCnt, vCnt];
+}
