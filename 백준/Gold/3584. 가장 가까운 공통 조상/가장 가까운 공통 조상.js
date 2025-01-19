ca3070377550685313722d2e2b2afdfb5c551e45
goodbyebada <90432681+goodbyebada@users.noreply.github.com>
2025-01-19 19:20:36 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 224 ms, Memory: 23816 KB -BaekjoonHub

--- 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.js
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.js
@@ -5,40 +5,40 @@
 
 const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 
-let inputs = require("fs").readFileSync(path).toString().trim().split("\n");
+let inputs = require("fs")
+  .readFileSync(path)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((elem) => elem.split(" ").map(Number));
 
 let countIdx = 1;
-let caseT = +inputs[0];
+let T = +inputs[0][0];
 
-while (caseT--) {
-  const nodeCount = +inputs[countIdx];
+while (T--) {
+  const nodeCount = inputs[countIdx][0];
 
-  const p = Array.from({ length: nodeCount + 1 }, (_) => -1);
+  const p = new Array(nodeCount + 1).fill(0).map((_, key) => key);
+  const visited = new Array(nodeCount + 1).fill(false);
 
   for (let i = countIdx; i < countIdx + nodeCount; i++) {
-    const [parent, child] = inputs[i].split(" ").map((elem) => Number(elem));
+    const [parent, child] = inputs[i];
     p[child] = parent;
   }
 
-  const [a, b] = inputs[countIdx + nodeCount]
-    .split(" ")
-    .map((elem) => Number(elem));
-
-  console.log(findCommonParent(a, b, p, nodeCount));
+  const [a, b] = inputs[countIdx + nodeCount];
+  console.log(findCommonParent(a, b, p, visited));
   countIdx += nodeCount + 1;
 }
 
-function findCommonParent(a, b, p, nodeCount) {
-  const visited = Array.from({ length: nodeCount + 1 }, () => false);
-
+function findCommonParent(a, b, p, visited) {
   let node1 = a;
   let node2 = b;
-  visited[node1] = true;
 
   while (1) {
-    if (p[node1] === -1) break;
-    node1 = p[node1];
     visited[node1] = true;
+    if (p[node1] === node1) break;
+    node1 = p[node1];
   }
 
   while (1) {
